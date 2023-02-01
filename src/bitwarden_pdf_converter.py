cfceba21db774a2dd1cd0ba7528f77b22c2c03b6
erhardma <54348970+erhardma@users.noreply.github.com>
2023-02-01 06:20:47 +0100

    Delete bitwarden_pdf_converter.py

--- src/bitwarden_pdf_converter.py
+++
@@ -1,8 +0,0 @@
-'''
-    Parses the bitwarden.json file and converts the PDFs to PNGs.
-'''
-import pdfkit
-
-from bitwarden_html_converter import parse_to_html
-
-pdfkit.from_string(parse_to_html(), './output.pdf')
